9a028ad804822333ec218a8215c3a75f3e142e8f
Marius Aiordachioaei <aiordachioaei.marius@gmail.com>
2023-02-16 20:57:44 +0200

    Add syntax highlighting

--- docs/cli-dsl-reference/overview.md
+++ docs/cli-dsl-reference/overview.md
@@ -4,5 +4,5 @@ title: Overview
 
 # Cli DSL
 
-Cli DSL ([Domain-specific language](https://en.wikipedia.org/wiki/Domain-specific_language)) is the language used by CLiMAT to generate toolchains. The syntax is meant to be tailored to the very problem CLiMAT is trying to solve, which is writing CLI  flexible interfaces.
+Cli DSL ([Domain-specific language](https://en.wikipedia.org/wiki/Domain-specific_language)) is the language used by CLiMAT to generate toolchains. The syntax is meant to be tailored to the very problem CLiMAT is trying to solve, which is writing flexible CLI interfaces.
 
---
+++ src/theme/prism-include-languages.js
@@ -0,0 +1,48 @@
+import siteConfig from '@generated/docusaurus.config';
+export default function prismIncludeLanguages(PrismObject) {
+  const {
+    themeConfig: {prism},
+  } = siteConfig;
+  const {additionalLanguages} = prism;
+  // Prism components work on the Prism instance on the window, while prism-
+  // react-renderer uses its own Prism instance. We temporarily mount the
+  // instance onto window, import components to enhance it, then remove it to
+  // avoid polluting global namespace.
+  // You can mutate PrismObject: registering plugins, deleting languages... As
+  // long as you don't re-assign it
+  globalThis.Prism = PrismObject;
+  additionalLanguages.forEach((lang) => {
+    // eslint-disable-next-line global-require, import/no-dynamic-require
+    require(`prismjs/components/prism-${lang}`);
+  });
+
+  PrismObject.languages.clidsl = {
+    'const': {
+      pattern: /(^|\s)const(^|\s)/,
+      alias: 'keyword',
+    },
+    'action': {
+      pattern: /(^|\s)action(^|\s)/,
+      alias: 'keyword'
+    },
+    'children': {
+      pattern: /(^|\s)children(^|\s)/,
+      alias: 'keyword'
+    },
+    'comment': {
+      pattern: /\/\/.*|\/\*[\s\S]*?(?:\*\/|$)/,
+      greedy: true
+    },
+    'builtin': {
+      pattern: /\$\(.*\)/,
+      greedy: true
+    },
+    'string': {
+      pattern: /(^|[^\\])"(?:\\.|[^\\"\r\n])*"(?!\s*:)/,
+      lookbehind: true,
+      greedy: true
+    },
+  }
+
+  delete globalThis.Prism;
+}
